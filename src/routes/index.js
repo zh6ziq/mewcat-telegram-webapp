0cdf9f5d8ce16e4dd7d49f566f81b2d1c2b75630
haziqruzi <71532216+zh6ziq@users.noreply.github.com>
2024-08-05 12:47:09 +0800

    added button to main page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/health', (req, res) => { 
+  return res.status(200).json({
+    status: 'success',
+    message: 'MewCat API is healthy.'
+  });
+});
+
 /* GET home page. */
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
