0bb4c379ea60b0627e2118de537afa15bc1f4f7d
haziqruzi <71532216+zh6ziq@users.noreply.github.com>
2024-08-05 15:15:40 +0800

    added nav

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,4 +13,13 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user', (req, res, next) => {
+  console.log(req.query, '---req');
+  res.render('home', {
+    title: 'User',
+    name: 'MewCat Bot',
+    desc: 'Mew mew...'
+   });
+});
+
 module.exports = router;
